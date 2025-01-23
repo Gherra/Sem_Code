2de02fd3dc7699b72b26806f2a7b7ab9903ddf8e
Raman Kumar <raman16894@gmail.com>
2025-01-22 23:23:12 -0800

    Add binSearch.cpp and .gitignore

---
+++ binSearch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<vector>
+
+
+
+using namespace std;
+
+int binSearch(vector<int> arr, int low, int high, int x) {
+    if (high >= low) { // Base case: when the search range is valid
+        int mid = (low + high) / 2; // Find the middle index
+        if (arr[mid] == x) { // If the middle element is the target, return its index
+            return mid;
+        } else if (arr[mid] > x) { // If the target is smaller, search the left half
+            return binSearch(arr, low, mid - 1, x);
+        } else { // If the target is larger, search the right half
+            return binSearch(arr, mid + 1, high, x);
+        }
+    }
+    return -1; // Base case: when the element is not found
+}
+
+int main(){
+    vector<int>arr = {1, 2, 3, 4, 5, 6, 7};
+    int reqIndex = binSearch(arr, 0, arr.size(), 6);
+
+    cout << reqIndex << '\n';
+    return 0;
+}
